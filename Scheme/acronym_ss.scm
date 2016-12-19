456bc8116cfd9abd2aa090e2169a241715db8cb7
cjledbetter <cjledbetter@gmail.com>
2016-12-18 22:22:55 -0500

    add simply.scm

---
+++ Scheme/acronym_ss.scm
@@ -0,0 +1,9 @@
+; #lang scheme
+; (require (planet dyoo/simply-scheme))
+
+(define (acronym phrase)
+  (accumulate word (every first (keep real-word? phrase))))
+
+(define (real-word? wd)
+  (not (member? wd '(a the an in of and for to with))))
+
