ad08a9928318adc201609e1b4a24b4e8946eb79f
chris ledbetter <chris ledbetter>
2016-12-28 20:55:01 -0500

    racket sllideshow

--- Scheme/slideshow.rkt
+++ Scheme/slideshow.rkt
@@ -7,4 +7,19 @@
   (define two-p (hc-append p p))
   (vc-append two-p two-p))
 (define (checker p1 p2)
-  (let ([p12 (
+  (let ([p12 (hc-append p1 p2)]
+        [p21 (hc-append p2 p1)])
+    (vc-append p12 p21)))
+; (checker (colorize (square 10) "red")
+;          (colorize (square 10) "black"))
+(define (checkerboard p)
+  (let* ([rp (colorize p "red")]
+         [bp (colorize p "black")]
+         [c (checker rp bp)]
+         [c4 (four c)])
+    (four c4)))
+;(checkerboard (square 10))
+(define (series mk)
+  (hc-append 4 (mk 5) (mk 10) (mk 20)))
+; (series circle)
+; (series square)
